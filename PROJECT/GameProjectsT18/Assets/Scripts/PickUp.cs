36f4affc5253474c37594cc18a57f4f836eef73d
LAPTOP-CQGREHP7\Yannick Sijtsma <yannick.sijtsma@student.howest.be>
2017-02-21 13:42:41 +0100

    Added Pickup mesh, Pickup Script, Making blog post today as well

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/PickUp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+    //FIELDS
+    private PlayerController _playerController;
+
+    //METHODs
+	
+    void Start()
+    {
+        GameObject gameControllerObj = GameObject.FindWithTag("Player");
+        if (gameControllerObj != null)
+        {
+            _playerController = gameControllerObj.GetComponent<PlayerController>();
+        }
+    }
+
+	void Update () {
+	    this.transform.Rotate(0f,0f,1.0f);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);  
+        }
+    }
+}
