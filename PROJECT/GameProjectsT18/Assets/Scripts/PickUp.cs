693d0fa6abc89c8126a2081c04291b53f9ddc71c
LAPTOP-CQGREHP7\Yannick Sijtsma <yannick.sijtsma@student.howest.be>
2017-02-24 11:50:22 +0100

    Fixed rotation and position for pickups

--- PROJECT/GameProjectsT18/Assets/Scripts/PickUp.cs
+++ PROJECT/GameProjectsT18/Assets/Scripts/PickUp.cs
@@ -5,12 +5,19 @@ public class PickUp : MonoBehaviour {
 
     //FIELDS
     private PlayerController _playerController;
+    private float _angle = 0;
+    public float Speed = 1;
+    public float Amplitude=0.5f;
+    private Vector3 _originalPos;
+    private Transform _transSelf;
 
     //METHODs
 	
     void Start()
     {
         GameObject gameControllerObj = GameObject.FindWithTag("Player");
+        _transSelf = this.transform;
+        _originalPos = _transSelf.position;
         if (gameControllerObj != null)
         {
             _playerController = gameControllerObj.GetComponent<PlayerController>();
@@ -18,8 +25,16 @@ public class PickUp : MonoBehaviour {
     }
 
 	void Update () {
-	    this.transform.Rotate(0f,0f,1.0f);
-	}
+        //_angle += Speed*Time.deltaTime;
+        //Vector3 pos = _originalPos;
+        //pos.y += Mathf.Sin(_angle)*Amplitude;
+        //_transSelf.Rotate(0f,0f,1.0f);
+        //_transSelf.position = pos;
+        _angle += Speed * Time.deltaTime;
+        _transSelf.Rotate(0f, 1.0f, 0f);
+        _transSelf.position = _originalPos;
+        _transSelf.Translate(0f, Mathf.Sin(_angle)*Amplitude,0f);
+    }
 
     void OnTriggerEnter(Collider other)
     {
