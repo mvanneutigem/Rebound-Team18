a20e805095ee0ebfe8ddcbe1d88f5698f3c9c0c0
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-02-24 11:18:50 +0100

    changed trampolines to work with refected vector above a set treshold

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/ReflectedTrampoline.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectedTrampoline : MonoBehaviour
+{
+    private float TrampolinePower = 1f;
+    private float UpPower = 10;
+    //pass up direction of trampoline as jumpvector
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            var normal = this.transform.up;
+            var inVelocity = other.GetComponent<PlayerController>().GetVelocity();
+            var reflectedVector = Vector3.Reflect(inVelocity, normal);
+            if (reflectedVector.y < 10.0f)
+            {
+                reflectedVector.y = UpPower;
+            }
+            else
+            {
+                reflectedVector.y *= 0.8f;
+            }
+            Debug.Log("add Up, " + reflectedVector);
+            Debug.Log("in, " + inVelocity);
+            other.GetComponent<PlayerController>().ApplyForce(reflectedVector * TrampolinePower);
+            Debug.Log("ontrampoline");
+        }
+    }
+}
