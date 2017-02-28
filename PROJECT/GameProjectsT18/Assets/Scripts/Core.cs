e92f7696b21494d6f428330729da38dbf03c1d63
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-02-28 11:16:31 +0100

    fixed core script, added meshes to prefab, played with color scheme

--- PROJECT/GameProjectsT18/Assets/Scripts/Core.cs
+++ PROJECT/GameProjectsT18/Assets/Scripts/Core.cs
@@ -6,33 +6,67 @@ public class Core : MonoBehaviour
 {
     private float _rotSpeed = 15.0f;
     private bool _isTriggerEntered = false;
-    private float _coreSpeed = 40.0f;
+    private float _coreSpeed = 100.0f;
+    private float _coreTime = 1.0f;
+    private float _distance = 2.0f;
+    private float _timer = 0f;
+    private GameObject _player;
+    private bool _setNewPos = true;
 
+    public Transform transformPosition;
 
-    public Transform PlayerTransform;
+    void Start()
+    {
+        transformPosition = transform;
+    }
     void Update()
     {
         //rotation
         transform.Rotate(Vector3.up, _rotSpeed * Time.deltaTime);
 
-        //checking distance
-        float distance = Vector3.Distance(transform.position, PlayerTransform.position);
         //if collected it follows
-        if (_isTriggerEntered == true && distance > 2)
+        if (_isTriggerEntered == true)
         {
+            //new script:
+            //transformPosition = transform;
+            if (_setNewPos)
+            {
+                var newPos = _player.transform.position;
+                newPos.z -= _distance;
+                transformPosition.position = newPos;
+                _setNewPos = false;
+            }
 
-            Vector3 dir = PlayerTransform.position - transform.position;
-            //direction
-            dir.Normalize();
+            var curDistance = _player.transform.position - transformPosition.position;
+            if (curDistance.magnitude > _distance)
+            {
+                curDistance.Normalize();
+                curDistance *= _distance;
+                transformPosition.position = Vector3.Lerp(transformPosition.position, _player.transform.position - curDistance, 0.5f);
+            }
 
-            transform.position += dir * _coreSpeed * Time.deltaTime;
+            transformPosition.RotateAround(_player.transform.position, _player.GetComponent<PlayerController>().GetUpVector(), _coreSpeed * Time.deltaTime);
 
-            //transform.LookAt(PlayerTransform);
-            //transform.position += transform.forward * 10 * Time.deltaTime;
+            transform.position = transformPosition.position;
+
+            //old script:
+            //Vector3 dir = PlayerTransform.position - transform.position;
+            ////direction
+            //dir.Normalize();
+
+            //transform.position += dir * _coreSpeed * Time.deltaTime;
+
+            ////transform.LookAt(PlayerTransform);
+            ////transform.position += transform.forward * 10 * Time.deltaTime;
         }
     }
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider other)
     {
-        _isTriggerEntered = true;
+        if (other.tag == "Player")
+        {
+            _isTriggerEntered = true;
+            _player = other.gameObject;
+        }
+            
     }
 }
