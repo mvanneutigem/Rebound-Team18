6f80797930a026ce80594a489f61f0224fff5ae8
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-02-28 20:14:14 +0100

    menu's + pausemenu, fixed smash, probably some other stuff as well

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/Buttons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    //FIELDS
+    public Transform panel;
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void loadScene(int sceneIndex)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        panel.gameObject.SetActive(false);
+    }
+}
