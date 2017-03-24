0d710fbc0fef15e4df98945bc1fa8c422394302e
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-03-24 10:38:58 +0100

    magnetic balls

--- PROJECT/GameProjectsT18/Assets/Scripts/Magnetize.cs
+++ PROJECT/GameProjectsT18/Assets/Scripts/Magnetize.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Magnetize : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class Magnetize : MonoBehaviour
+{
+    public enum Type { Attract, Repel }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float RepelForce = 1000.0f;
+
+    [SerializeField]
+    private float MinimumDistance = 1.0f;
+
+    [SerializeField]
+    private Type MagnetizeType = Type.Repel;
+
+    private GameObject[] _magneticObject;
+    private Rigidbody _rigidbody;
+
+    void Awake()
+    {
+        _magneticObject = GameObject.FindGameObjectsWithTag("Magnetic");
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (_magneticObject != null)
+        {
+            for(int i = 0; i < _magneticObject.Length; ++i)
+            {
+                Vector3 difference;
+                if (MagnetizeType == Type.Repel)
+                    difference = transform.position - _magneticObject[i].transform.position;
+                else
+                    difference = _magneticObject[i].transform.position - transform.position;
+
+                if (difference.magnitude <= MinimumDistance)
+                {
+                    _rigidbody.AddForce(difference * RepelForce * Time.deltaTime);
+                }
+            }
+           
+        }
+    }
 }
