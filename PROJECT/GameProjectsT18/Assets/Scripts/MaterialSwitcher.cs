bb34023ddad82485bc7bb9fd5ec6bf0318064e3e
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-03-28 22:47:31 +0200

    matswitcher and stuff

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/MaterialSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwitcher : MonoBehaviour {
+
+    public Material newMaterialRef;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Renderer>().material = newMaterialRef;
+        }
+    }
+}
