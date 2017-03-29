53603382d14e91d68267be47dc6f6f60d6cffb29
kiamvdd <23549713+kiamvdd@users.noreply.github.com>
2017-03-29 15:01:42 +0200

    Updated player script

--- PROJECT/GameProjectsT18/Assets/Scripts/PhysicsPlayerController.cs
+++ PROJECT/GameProjectsT18/Assets/Scripts/PhysicsPlayerController.cs
@@ -170,6 +170,7 @@ public class PhysicsPlayerController : MonoBehaviour
     public void SetForwardDir(Vector3 forward)
     {
         _moveDirForward = forward;
+        _playerRigidBody.velocity = _moveDirForward.normalized * _playerRigidBody.velocity.magnitude;
     }
 
     public Vector3 GetForwardDir()
