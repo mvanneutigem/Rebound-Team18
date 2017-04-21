052e90d45a5d90dff1a74e41c7dcbb88d98fc7ef
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-04-21 11:27:49 +0200

    fixed the projection, super simple tutorial level, text in the ui

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/Tips.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tips : MonoBehaviour
+{
+
+    public Text[] textArray;
+    public float[] positionArray;
+    public float[] distanceArray;
+    private GameObject _player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    for (var i = 0; i < textArray.Length; ++i)
+	    {
+            textArray[i].gameObject.SetActive(false);
+            if (_player.transform.position.z > positionArray[i] && _player.transform.position.z  < positionArray[i] + distanceArray[i] )
+	        {
+	            textArray[i].gameObject.SetActive(true);
+
+	        }
+	    }
+	}
+}
