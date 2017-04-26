078e998a565990fb171f8765e1c4935d885705a5
Tim Backman <tim.backman@student.howest.be>
2017-04-26 09:19:37 +0200

    Level fix death planes

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private Text _scoreText;
+    private Text _timeText;
+    private int _score;
+    private float _timer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _scoreText = GameObject.FindWithTag("ScoreText").GetComponent<Text>();
+        _timeText = GameObject.FindWithTag("TimeText").GetComponent<Text>();
+        _score = 0;
+        _timer = 120;
+    }
+
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        _timeText.text = _timer.ToString("n2");
+    }
+    void UpdateScore()
+    {
+        _scoreText.text = _score.ToString();
+        PlayerPrefs.SetInt("Score", _score);
+    }
+
+    public void AddScore(int newScore)
+    {
+        _score += newScore;
+        UpdateScore();
+    }
+
+    public float GetTime()
+    {
+        return _timer;
+    }
+}
