920931dde404c1f76872fff9edd6ebac6714d53e
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-05-05 11:58:15 +0200

    scale stuff, started squash stuff

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/SquashScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquashScript : MonoBehaviour {
+
+    public void Squash()
+    {
+        var scale = transform.localScale;
+        scale.y = 0.5f;
+        transform.localScale = scale;
+    }
+
+    public void Stretch()
+    {
+        var scale = transform.localScale;
+        scale.y = 1.5f;
+        transform.localScale = scale;
+    }
+
+    public void NoSquash()
+    {
+        var scale = transform.localScale;
+        scale.y = 1.0f;
+        transform.localScale = scale;
+    }
+}
