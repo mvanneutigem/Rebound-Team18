73f31f14d28d6e421969814ebc2e88c779aaf55b
kiamvdd <23549713+kiamvdd@users.noreply.github.com>
2017-05-06 12:54:05 +0200

    Added bounce sound

---
+++ PROJECT/GameProjectsT18/Assets/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource[] _sources;
+	// Use this for initialization
+	void Start ()
+	{
+        _sources = GetComponents<AudioSource>();
+	}
+
+    public void PlaySound(string name)
+    {
+        for (int i = 0; i < _sources.Length; i++)
+        {
+            if (name == _sources[i].clip.name)
+            {
+                _sources[i].Play();
+            }
+        }
+    }
+}
