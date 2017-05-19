167b78d9d6b1838fbf21e976ad28bdee16cfb8f1
Yannick Sijtsma <yannick.sijtsma@student.howest.be>
2017-05-19 12:36:39 +0200

    Fixed postprocessing not working for all the levels, changed the filter from romantic to horror, less bright but still nice saturation

---
+++ PROJECT/GameProjectsT18/Assets/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource[] _sources;
+	// Use this for initialization
+	void Start ()
+	{
+        _sources = GetComponents<AudioSource>();
+	}
+
+    //for quick sounds that may layer over the top of each other
+    public void PlaySFX(string name)
+    {
+        for (int i = 0; i < _sources.Length; i++)
+        {
+            if (_sources[i].clip)
+            {
+                if (name == _sources[i].clip.name)
+                {
+                    _sources[i].PlayOneShot(_sources[i].clip);
+                }
+            }
+        }
+    }
+
+    //for longer sounds, like music or ambient noise
+    public void PlayLong(string name)
+    {
+        for (int i = 0; i < _sources.Length; i++)
+        {
+            if (_sources[i].clip)
+            {
+                if (name == _sources[i].clip.name)
+                {
+                    _sources[i].Play();
+                }
+            }
+        }
+    }
+}
---
+++ PROJECT/GameProjectsT18/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private Text _scoreText;
+    private Text _timeText;
+    private int _score;
+    private float _timer;
+    private bool _gameStarted = false;
+
+    public void StartGame()
+    {
+        _gameStarted = true;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        _scoreText = GameObject.FindWithTag("ScoreText").GetComponent<Text>();
+        _timeText = GameObject.FindWithTag("TimeText").GetComponent<Text>();
+        _score = 0;
+        _timer = 120;
+    }
+
+    void Update()
+    {
+        if (_gameStarted)
+        {
+            _timer -= Time.deltaTime;
+        }
+        _timeText.text = _timer.ToString("n2");
+
+    }
+    void UpdateScore()
+    {
+        _scoreText.text = _score.ToString();
+        PlayerPrefs.SetInt("Score", _score);
+    }
+
+    public void AddScore(int newScore)
+    {
+        _score += newScore;
+        UpdateScore();
+    }
+
+    public float GetTime()
+    {
+        return _timer;
+    }
+}
