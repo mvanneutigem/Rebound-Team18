c576f128a18206e4e051410264e059d64596f357
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-05-23 21:45:13 +0200

    unlock levels and level 3-2 update

--- PROJECT/GameProjectsT18/Assets/Scripts/SwitchWorlds.cs
+++ PROJECT/GameProjectsT18/Assets/Scripts/SwitchWorlds.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchWorlds : MonoBehaviour
 {
@@ -10,6 +12,32 @@ public class SwitchWorlds : MonoBehaviour
     private int currentWorld = 1;
     private int maxworld = 2;
 
+    void Start()
+    {
+        SetButtons(panel1);
+    }
+
+    public void UnlockAll()
+    {
+        PlayerPrefs.SetInt("Highscore", 1000000);
+        SetButtons(panel1);
+    }
+
+    void SetButtons(GameObject panel)
+    {
+        int baseScore = 10000;
+        for (int i = 0; i < 5; ++i)
+        {
+            if (PlayerPrefs.GetInt("Highscore") < baseScore * (i + (currentWorld-1) * 5))
+            {
+                panel.transform.GetChild(i+2).GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                panel.transform.GetChild(i+2).GetComponent<Button>().interactable = true;
+            }
+        }
+    }
     private void SwitchWorld()
     {
         switch (currentWorld)
@@ -17,10 +45,12 @@ public class SwitchWorlds : MonoBehaviour
             case 1:
                 panel1.SetActive(true);
                 panel2.SetActive(false);
+                SetButtons(panel1);
                 break;
             case 2:
                 panel1.SetActive(false);
                 panel2.SetActive(true);
+                SetButtons(panel2);
                 break;
         }
     }
