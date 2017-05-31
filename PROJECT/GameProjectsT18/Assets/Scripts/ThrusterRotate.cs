c0e9f77b6ed6b25958347855bf04bfa0daa80fb2
Yannick Sijtsma <yannick.sijtsma@student.howest.be>
2017-05-31 17:04:20 +0200

    Made new particle and script for my floating platforms, fourth level, level 3-4 now fully playable and finished

---
+++ PROJECT/GameProjectsT18/Assets/Scripts/ThrusterRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterRotate : MonoBehaviour {
+    private Transform _transSelf;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        _transSelf = this.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _transSelf.Rotate(0f, Speed, 0f);
+    }
+}
