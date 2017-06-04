7d7586c173f6b55d83cfea3fcd0379446b32e7bc
kiamvdd <23549713+kiamvdd@users.noreply.github.com>
2017-06-05 01:28:09 +0200

    Added god mode

--- PROJECT/GameProjectsT18/Assets/Scripts/Death.cs
+++ PROJECT/GameProjectsT18/Assets/Scripts/Death.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Death : MonoBehaviour {
+public class Death : MonoBehaviour
+{
+    private bool _enabled = true;
+    public void SetEnabled(bool enabled)
+    {
+        _enabled = enabled;
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && _enabled)
         {
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentScene);
